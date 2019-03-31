f83bdf928ed3cc24ebce68e89d1cad943b1d9ee5
Sxnty <sxntycsgo@gmail.com>
2019-03-31 19:32:05 -0300

    Learning list (part 2)

--- list_while.py
+++ list_while.py
@@ -1,9 +1,55 @@
-my_list = ["Lechuga", "Pan", "Coca Cola", "Jamon"]
+my_list = []
+input_user = ""
+j = 0 # while menu #
+while j < 1:
+    print("1.Lista de compra")
+    print("2.Salir")
+    selection = int(input("Seleccione una opcion:"))
 
-len_list = len(my_list)
-actual_indice = 0
+    if selection == 1:
+        j = 1
+        while input_user != "FIN":
+            input_user = input("Que deseas comprar? (Dijita FIN para terminar) : ").upper()
+            if input_user != "FIN":
+                my_list.append(input_user)
 
-while  actual_indice < len_list:
-    print("Tengo que comprar {}".format(my_list[actual_indice]))
-    actual_indice += 1
-print("Esta es mi lista de compra")
+        len_list = len(my_list)
+        actual_indice = 0
+
+        while  actual_indice < len_list:
+            print("Tengo que comprar {}".format(my_list[actual_indice]))
+            actual_indice += 1
+        print("Esta es mi lista de compra")
+
+    if selection == 2:
+        j = 1
+        print("Programa terminado")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
