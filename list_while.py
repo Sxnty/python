986d39cd3d129be52f49100a1771214e36e64a98
Sxnty <sxntycsgo@gmail.com>
2019-03-31 18:23:56 -0300

    Learning list (part 1)

---
+++ list_while.py
@@ -0,0 +1,9 @@
+my_list = ["Lechuga", "Pan", "Coca Cola", "Jamon"]
+
+len_list = len(my_list)
+actual_indice = 0
+
+while  actual_indice < len_list:
+    print("Tengo que comprar {}".format(my_list[actual_indice]))
+    actual_indice += 1
+print("Esta es mi lista de compra")
